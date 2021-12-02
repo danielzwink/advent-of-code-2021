a8b75ada0413c62daed124504a238f78d87c9be0
Daniel Zwink <daniel.zwink@mail.schwarz>
2021-12-02 17:13:24 +0100

    Solve puzzle 02

---
+++ cmd/puzzles/02/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"advent-of-code-2021/pkg/util"
+	"bufio"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Printf("Part 1: %d\n", part1())
+	fmt.Printf("Part 2: %d\n", part2())
+}
+
+func part1() int {
+	file := util.OpenFile("02")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var position, depth int
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		command := line[0]
+		change, _ := strconv.Atoi(line[1])
+
+		switch command {
+		case "forward":
+			position += change
+		case "down":
+			depth += change
+		case "up":
+			depth -= change
+		}
+
+	}
+
+	return position * depth
+}
+
+func part2() int {
+	file := util.OpenFile("02")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var position, depth, aim int
+
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), " ")
+		command := line[0]
+		change, _ := strconv.Atoi(line[1])
+
+		switch command {
+		case "forward":
+			position += change
+			depth += aim * change
+		case "down":
+			aim += change
+		case "up":
+			aim -= change
+		}
+
+	}
+
+	return position * depth
+}
